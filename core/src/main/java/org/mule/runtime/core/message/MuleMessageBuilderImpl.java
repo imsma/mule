f681248654c60292520ca2f1d9321db474ed41ff
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2016-06-15 19:12:00 -0300

    MULE-9856: Create MuleMessage builder

--- core/src/main/java/org/mule/runtime/core/message/MuleMessageBuilderImpl.java
+++ core/src/main/java/org/mule/runtime/core/message/MuleMessageBuilderImpl.java
@@ -6,10 +6,18 @@
  */
 package org.mule.runtime.core.message;
 
+import static java.lang.String.format;
+import static org.mule.runtime.core.util.UUID.getUUID;
+
 import org.mule.runtime.api.message.MuleMessage;
 import org.mule.runtime.api.message.MuleMessageBuilder;
 import org.mule.runtime.api.metadata.DataType;
 import org.mule.runtime.core.DefaultMuleMessage;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * Provides a way to build immutable {@link MuleMessage} objects.
@@ -19,17 +27,44 @@ import org.mule.runtime.core.DefaultMuleMessage;
 public class MuleMessageBuilderImpl<T> implements MuleMessageBuilder<T>
 {
 
-    private Object payload;
+    private String id;
+    private T payload;
     private DataType<T> dataType;
+    private Map<String, Serializable> properties = new HashMap<>();
 
     @Override
-    public MuleMessage build()
+    public MuleMessage<T, ? extends Serializable> build()
     {
-        return new DefaultMuleMessage(payload, dataType);
+        if (payload == null)
+        {
+            throw new IllegalArgumentException("MuleMessage cannot have 'null' payload.");
+        }
+
+        final DataType<T> payloadDataType = DataType.<T> createFromObject(payload);
+        if (dataType == null)
+        {
+            dataType = payloadDataType;
+        }
+        else
+        {
+            if (!dataType.isCompatibleWith(payloadDataType))
+            {
+                throw new IllegalArgumentException(format("Given 'dataType' (%s) is not compatible with the given payload type (%s).", dataType.toString(), payloadDataType.toString()));
+            }
+        }
+
+        final DefaultMuleMessage message = new DefaultMuleMessage(payload, dataType);
+        message.setUniqueId(id != null ? id : getUUID());
+        for (Entry<String, Serializable> propertyEntry : properties.entrySet())
+        {
+            message.setOutboundProperty(propertyEntry.getKey(), propertyEntry.getValue());
+        }
+
+        return (MuleMessage<T, ? extends Serializable>) message;
     }
 
     @Override
-    public MuleMessageBuilder<T> setPayload(Object payload)
+    public MuleMessageBuilder<T> setPayload(T payload)
     {
         this.payload = payload;
         return this;
@@ -39,6 +74,48 @@ public class MuleMessageBuilderImpl<T> implements MuleMessageBuilder<T>
     public MuleMessageBuilder<T> setDataType(DataType<T> dataType)
     {
         this.dataType = dataType;
+        return this;
+    }
+
+    @Override
+    public MuleMessageBuilder<T> setId(String id)
+    {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public MuleMessageBuilder<T> addProperties(Map<String, Serializable> properties)
+    {
+        this.properties.putAll(properties);
+        return this;
+    }
+
+    @Override
+    public MuleMessageBuilder<T> addProperty(String key, Serializable value)
+    {
+        properties.put(key, value);
+        return this;
+    }
+
+    @Override
+    public MuleMessageBuilder<T> removeProperty(String key)
+    {
+        properties.remove(key);
+        return this;
+    }
+
+    @Override
+    public MuleMessageBuilder<T> copy(MuleMessage<T, ? extends Serializable> copyFrom)
+    {
+        // properties.clear();
+        // properties.putAll(copyFrom.getAttributes());
+
+        // id = copyFrom.getId();
+
+        payload = copyFrom.getPayload();
+        dataType = (DataType<T>) copyFrom.getDataType();
+
         return this;
     }
 }
